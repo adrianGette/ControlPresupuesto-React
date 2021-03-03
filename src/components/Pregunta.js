29918f3c0415cfed619efb3c32e0fce4a4121d60
Adrian Gette <adrian.gette01@gmail.com>
2021-03-03 12:58:49 -0300

    Fin App basica en React

---
+++ src/components/Pregunta.js
@@ -0,0 +1,65 @@
+import React, { Fragment, useState } from 'react';
+import Error from './Error';
+import PropTypes from 'prop-types';
+
+const Pregunta = ({ guardarPresupuesto, guardarRestante, actualizarPregunta }) => {
+
+    // Definir el state
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    // Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad( parseInt(e.target.value, 10) )
+    }
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+        if( cantidad < 1 || isNaN( cantidad ) ) {
+            guardarError(true);
+            return;
+        }
+
+        // Si se pasa la validacion
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    }
+
+    return ( 
+        <Fragment>
+            <h2>Cuál es tu presupuesto?</h2>
+
+            { error ? <Error mensaje="El Presupuesto es Incorrecto" /> : null }
+
+            <form
+                onSubmit = { agregarPresupuesto }
+            >
+                <input
+                    type = "number"
+                    className = "u-full-width"
+                    placeholder = "Cuál es tu presupuesto?"
+                    onChange = { definirPresupuesto }
+                />
+
+                <input
+                    type = "submit"
+                    className = "button-primary u-full-width"
+                    value = "Definir presupuesto"
+                />
+            </form>
+        </Fragment>
+    );
+}
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired
+}
+ 
+export default Pregunta;
